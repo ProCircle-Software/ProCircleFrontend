9790c4effea27d6f6b9862114b50ae2d8e9a466f
Theo Fortune <theofortunefunction@gmail.com>
2025-04-25 01:37:24 +0100

    nav

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import './navbar.css'
+import React from "react";
+import "./navbar.css";
+
+const NavLinks = () => {
+  return (
+    <div className="nav-links">
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
+      <a href="#features">Features</a>
+      <a href="#team">Team</a>
+    </div>
+  );
+};
 
 const NavBar = () => {
   return (
-    <nav>NavBar</nav>
-  )
-}
+    <nav className="nav">
+      <p className="logo">ProCircle</p>
+      <NavLinks />
+      <button className="nav-button primary-button">Explore</button>
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
